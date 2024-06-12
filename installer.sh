bb47152f933ff0cd06de8293d6811d1ebd9f5406
DeadPoolCS2 <168067516+DeadPoolCS2@users.noreply.github.com>
2024-06-12 22:27:08 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 print_header() {
-    clear
     echo "  _   _             ____            "
     echo " | \ | | ___   ___ | __ )  ___  ___ "
     echo " |  \| |/ _ \ / _ \|  _ \ / _ \/ _ \\"
